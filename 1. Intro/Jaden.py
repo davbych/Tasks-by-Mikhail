8af3cf930f6573415c93bb50c37c287ac0b23e94
Davbych <skripkinm17@gmail.com>
2025-09-19 20:41:41 +0700

    Add files via upload

--- 1. Intro/Jaden.py
+++ 1. Intro/Jaden.py
@@ -1,5 +1,5 @@
 def to_jaden_case(text):
     words = text.split()
-    jaden_words = [word.capitalize() for word in words]
+    jaden_words = [i.capitalize() for i in words]
     return ' '.join(jaden_words)
 print(to_jaden_case(input()))
